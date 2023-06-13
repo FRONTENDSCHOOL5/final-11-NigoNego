950e580c59c52ae021c863041ffdb6930b48047e
snowkwon420 <snowkwon420@gmail.com>
2023-06-13 10:22:24 +0900

    💄 Design : CSS, 사용자 UI 디자인 변경 #11

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -1,53 +1,51 @@
-import React from 'react';
-import StyleSplashPage from './SplashStyle.jsx'
-
+import React from "react";
+import StyleSplashPage from "./SplashStyle.jsx";
 
 function SplashPage() {
   return (
     <>
-     <StyleSplashPage>
-
-       <div className="lego red">
-        <div className="left"></div>
-        <div className="right"></div>
-        <div className="container-top">
-          <div className="top">
-            <div className="dot d-n1"></div>
-            <div className="dot d-n2"></div>
-            <div className="dot d-n3"></div>
-            <div className="dot d-n4"></div>
+      <StyleSplashPage>
+        <div className="lego red">
+          <div className="left"></div>
+          <div className="right"></div>
+          <div className="container-top">
+            <div className="top">
+              <div className="dot d-n1"></div>
+              <div className="dot d-n2"></div>
+              <div className="dot d-n3"></div>
+              <div className="dot d-n4"></div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="lego blue">
-        <div className="left"></div>
-        <div className="right"></div>
-        <div className="container-top">
-          <div className="top">
-            <div className="dot d-n1"></div>
-            <div className="dot d-n2"></div>
-            <div className="dot d-n3"></div>
-            <div className="dot d-n4"></div>
+        <div className="lego blue">
+          <div className="left"></div>
+          <div className="right"></div>
+          <div className="container-top">
+            <div className="top">
+              <div className="dot d-n1"></div>
+              <div className="dot d-n2"></div>
+              <div className="dot d-n3"></div>
+              <div className="dot d-n4"></div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="lego yellow">
-        <div className="left"></div>
-        <div className="right"></div>
-        <div className="container-top">
-          <div className="top">
-            <div className="dot d-n1"></div>
-            <div className="dot d-n2"></div>
-            <div className="dot d-n3"></div>
-            <div className="dot d-n4"></div>
+        <div className="lego yellow">
+          <div className="left"></div>
+          <div className="right"></div>
+          <div className="container-top">
+            <div className="top">
+              <div className="dot d-n1"></div>
+              <div className="dot d-n2"></div>
+              <div className="dot d-n3"></div>
+              <div className="dot d-n4"></div>
+            </div>
           </div>
         </div>
-      </div>
       </StyleSplashPage>
-  </>
-  )
+    </>
+  );
 }
 
 export default SplashPage;
