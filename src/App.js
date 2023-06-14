5802774d4019c5f03b797b0525f26edb50ce72e1
Oh5Yeonju <oh5yeonju@gmail.com>
2023-06-14 10:48:32 +0900

    🐛 Fix: App.js 이슈 수정 #16

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 // import Routing from './routes/Routing';
 import GlobalStyle from './styles/globalstyle';
 import {
-    LBtn,
-    LdisabledBtn,
-    MBtn,
-    MdisabledBtn,
-    MActivBtn,
-    MSBtn,
-    MSdisabledBtn,
-    SBtn,
-    SactivBtn,
+  LBtn,
+  LdisabledBtn,
+  MBtn,
+  MdisabledBtn,
+  MActivBtn,
+  MSBtn,
+  MSdisabledBtn,
+  SBtn,
+  SactivBtn,
 } from './components/common/Button/Button';
 
 function App() {
   return (
-      <>
-          <GlobalStyle />
-          {/* <Routing /> */}
-          <LBtn />
-          <LdisabledBtn />
-          <MBtn />
-          <MdisabledBtn />
-          <MActivBtn />
-          <MSBtn />
-          <MSdisabledBtn />
-          <SBtn />
-          <SactivBtn />
-      </>
+    <>
+      <GlobalStyle />
+      {/* <Routing /> */}
+      <LBtn />
+      <LdisabledBtn />
+      <MBtn />
+      <MdisabledBtn />
+      <MActivBtn />
+      <MSBtn />
+      <MSdisabledBtn />
+      <SBtn />
+      <SactivBtn />
+    </>
   );
 }
 export default App;
