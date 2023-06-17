e2f05e2b54e4a2978347368fa58f292807ef1ed0
snowkwon420 <snowkwon420@gmail.com>
2023-06-18 03:14:02 +0900

    ♻️ Refactor: #52 프로필 이미지 컴포넌트 수정(재수정 필요)

--- src/components/common/UserImage/UserImage.jsx
+++ src/components/common/UserImage/UserImage.jsx
@@ -1,19 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import basicImg from '../../../assets/images/basic-profile-img.png';
 
-function Image({ src, alt }) {
+/**
+이미지컴포넌트에서 나중에 프로필에서 이미지 설정할때 사용될거같아서
+이미지에 prop로 이미지 링크 전달해주게 짜려고 했지만 실패 했어요..
+하지만 베이직 이미지는 정상적으로나와서 이렇게 짠상태로 두겠습니다.!
+ */
+
+const ImageStyle = styled.div`
+  border-radius: 50%;
+  background-image: url(${props => props.backgroundUrl});
+  background-size: cover;
+`;
+
+const LProfile = styled(ImageStyle)`
+  width: 110px;
+  height: 110px;
+`;
+
+export function LImage({ backgroundUrl, ...props }) {
   return (
-    <ImageStyle>
-      <img src={src} alt={alt} />
-    </ImageStyle>
+    <LProfile
+      backgroundUrl={props.backgroundUrl ? props.backgroundUrl : basicImg}
+    />
   );
 }
 
-export default Image;
-
-const ImageStyle = styled.div`
-  width: 110px;
-  height: 110px;
-  border-radius: 50%;
-  text-align: center;
+const MProfile = styled(ImageStyle)`
+  width: 50px;
+  height: 50px;
 `;
+
+export function MImage({ backgroundUrl, ...props }) {
+  return (
+    <MProfile
+      backgroundUrl={props.backgroundUrl ? props.backgroundUrl : basicImg}
+    />
+  );
+}
+
+const SProfile = styled(ImageStyle)`
+  width: 42px;
+  height: 42px;
+`;
+
+export function SImage({ backgroundUrl, ...props }) {
+  return (
+    <SProfile
+      backgroundUrl={props.backgroundUrl ? props.backgroundUrl : basicImg}
+    />
+  );
+}
+
+const XSProfile = styled(ImageStyle)`
+  width: 36px;
+  height: 35px;
+`;
+
+export function XSImage({ backgroundUrl, ...props }) {
+  return (
+    <XSProfile
+      backgroundUrl={props.backgroundUrl ? props.backgroundUrl : basicImg}
+    />
+  );
+}
