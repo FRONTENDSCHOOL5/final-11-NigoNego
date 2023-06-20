a01d646674343e9952cb6ba3e60f17158247d597
Oh5Yeonju <oh5yeonju@gmail.com>
2023-06-20 17:17:12 +0900

    🐛 Fix: #67 이미지 미리보기 오류 수정

--- src/pages/PostPage/PostUpload/PostUpload.jsx
+++ src/pages/PostPage/PostUpload/PostUpload.jsx
@@ -18,17 +18,17 @@ export default function PostUpload() {
     const file = e.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
-
     // 데이터 호출, 비동기에 대해서 조금은 알아보는 것이 좋을 것 같아요!
 
     // 이미지를 등록하면 이미지 filename을 응답받는 api를 호출할꺼야!
     axios({
-      method: 'post',
+      method: 'POST',
       url: 'https://api.mandarin.weniv.co.kr/image/uploadfile',
       data: formData,
     }).then(result => {
       console.log('요청성공');
-      setImage(result.data.path);
+      setImage(`https://api.mandarin.weniv.co.kr/${result.data.filename}`);
+      console.log(image);
       // 여기서 받은 응답을 어딘가에 저장하기! setImage(filename) -> 미리보기 구현은 끝!
 
       // 그 이미지 파일이름을 받으면 그거를 사용해서 미리보기 구현한다.
@@ -77,7 +77,7 @@ export default function PostUpload() {
       {/* <UploadButton /> */}
       <UploadButtonStyle>
         <label htmlFor="input" />
-        <input id="input" type="file" onChange={e => handleImageUpload(e)} />
+        <input id="input" type="file" onChange={handleImageUpload} />
       </UploadButtonStyle>
     </>
   );
