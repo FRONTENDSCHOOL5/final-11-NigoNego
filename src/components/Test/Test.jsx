98a76d5246203cce99aed5fc5da9b2bfeeab5c27
Oh5Yeonju <oh5yeonju@gmail.com>
2023-06-20 14:43:27 +0900

    이미지업로드

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../common/Navbar/Navbar';
-
+import PostUpload from '../../pages/PostPage/PostUpload/PostUpload';
 export default function Test() {
   return (
     <div>
       <Navbar />
+      <PostUpload />
     </div>
   );
 }
