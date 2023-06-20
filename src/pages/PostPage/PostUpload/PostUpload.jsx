d4e050284e7afcd46626d40fac12d461402d1a5d
Oh5Yeonju <oh5yeonju@gmail.com>
2023-06-20 18:03:47 +0900

    ✨ Feat: #67 form onSubmit 기능 추가

--- src/pages/PostPage/PostUpload/PostUpload.jsx
+++ src/pages/PostPage/PostUpload/PostUpload.jsx
@@ -6,21 +6,16 @@ import buttonImg from '../../../assets/images/upload-file.svg';
 import axios from 'axios';
 
 export default function PostUpload() {
-  // 토큰 가져오는 거 잊으면 안돼요!! -> 토큰인 로그인할 때 받을 수 있음.
-  // 리액트에서 바뀌는 부분, 바뀌어야 하는 부분은 모두 state!
-  // const token = '받은 토큰';
   const [content, setContent] = useState('');
   const [image, setImage] = useState('');
-  // const image = '';
 
   const handleImageUpload = e => {
     e.preventDefault();
+
     const file = e.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
-    // 데이터 호출, 비동기에 대해서 조금은 알아보는 것이 좋을 것 같아요!
 
-    // 이미지를 등록하면 이미지 filename을 응답받는 api를 호출할꺼야!
     axios({
       method: 'POST',
       url: 'https://api.mandarin.weniv.co.kr/image/uploadfile',
@@ -29,56 +24,57 @@ export default function PostUpload() {
       console.log('요청성공');
       setImage(`https://api.mandarin.weniv.co.kr/${result.data.filename}`);
       console.log(image);
-      // 여기서 받은 응답을 어딘가에 저장하기! setImage(filename) -> 미리보기 구현은 끝!
-
-      // 그 이미지 파일이름을 받으면 그거를 사용해서 미리보기 구현한다.
     });
   };
 
-  // const handleSubmit = () => {
-  //   // 게시글 작성 api 호출
-  //   try {
-  //     const res = axios({
-  //       method: 'post',
-  //       url: `https://api.mandarin.weniv.co.kr/post`,
-  //       body: {
-  //         post: {
-  //           content: content,
-  //           image: `https://api.mandarin.weniv.co.kr/${image}`,
-  //         },
-  //       },
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //         'Content-type': 'application/json',
-  //       },
-  //     });
-  //     // 여기서 응답 확인 -> 어떤 응답이 왔는지, 성공 유무
-  //     console.log(res);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('123');
+    // 게시글 작성 api 호출
+    // try {
+    //   const res = axios({
+    //     method: 'post',
+    //     url: `https://api.mandarin.weniv.co.kr/post`,
+    //     body: {
+    //       post: {
+    //         content: content,
+    //         image: `https://api.mandarin.weniv.co.kr/${image}`,
+    //       },
+    //     },
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //       'Content-type': 'application/json',
+    //     },
+    //   });
+    //   // 여기서 응답 확인 -> 어떤 응답이 왔는지, 성공 유무
+    //   console.log(res);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  };
 
   return (
     <>
-      <HeaderUploadNav />
-      <PostUploadWrapper>
-        <ProfileWrapper>
-          <SImage />
-        </ProfileWrapper>
-        <textarea
-          className="inputPost"
-          placeholder="게시글 입력하기…"
-          value={content}
-          onChange={e => setContent(e.target.value)}
-        />
-        {image.length > 0 && <img src={image} alt="" />}
-      </PostUploadWrapper>
-      {/* <UploadButton /> */}
-      <UploadButtonStyle>
-        <label htmlFor="input" />
-        <input id="input" type="file" onChange={handleImageUpload} />
-      </UploadButtonStyle>
+      <form onSubmit={handleSubmit}>
+        <HeaderUploadNav />
+        <PostUploadWrapper>
+          <ProfileWrapper>
+            <SImage />
+          </ProfileWrapper>
+          <textarea
+            className="inputPost"
+            placeholder="게시글 입력하기…"
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
+          {image.length > 0 && <img src={image} alt="" />}
+        </PostUploadWrapper>
+        {/* <UploadButton /> */}
+        <UploadButtonStyle>
+          <label htmlFor="input" />
+          <input id="input" type="file" onChange={handleImageUpload} />
+        </UploadButtonStyle>
+      </form>
     </>
   );
 }
