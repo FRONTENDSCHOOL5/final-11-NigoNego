68b5ad2b5c29a74adfbad2cf8213401667f1c6ea
leesua <thkWkddl100@gmail.com>
2023-06-21 17:09:40 +0900

    합치기

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MyProfilePage from '../../pages/ProfilePage/MyProfilePage/MyProfilePage';
+import AddProductImg from './../../pages/ProductPage/AddProductImg';
 export default function Test() {
   return (
     <div>
-      <MyProfilePage />
+      <AddProductImg />
     </div>
   );
 }
