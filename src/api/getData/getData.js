b3729ebab1f370a158a1055f82a872ea5629a353
leesua <thkWkddl100@gmail.com>
2023-06-23 16:05:50 +0900

    ✨ Feat:  getApi 연동기능 추가

--- src/api/getData/getData.js
+++ src/api/getData/getData.js
@@ -1,7 +1,7 @@
 import { getDataBase } from "../Api";
 
-export const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGFkMDkxYjJjYjIwNTY2MzM1ZjVmMCIsImV4cCI6MTY5MjAwMjk4NiwiaWF0IjoxNjg2ODE4OTg2fQ.IXRWQpeGB-5D3U3iN4FSKNf2F92wGVA_FLw4SpqLc20';
+// export const token =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGFkMDkxYjJjYjIwNTY2MzM1ZjVmMCIsImV4cCI6MTY5MjAwMjk4NiwiaWF0IjoxNjg2ODE4OTg2fQ.IXRWQpeGB-5D3U3iN4FSKNf2F92wGVA_FLw4SpqLc20';
 
 
 // homepost
