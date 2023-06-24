44ad4b8af9d5c8fd02c7fafe3750e8b2d676c3a2
min <rlaalswl1238@naver.com>
2023-06-24 18:44:22 +0900

    🐛 Fix :  84 Routing.jsx 에서 Login 토큰시 페이지 보이게 수정

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import StartSplash from '../pages/SplashPage/StartSplash';
 import LoginMain from '../pages/LoginMain/LoginMain';
 import LoginPage from '../pages/LoginPage/LoginPage';
@@ -18,30 +18,42 @@ import Test from '../components/Test/Test';
 import JoinMember from '../pages/JoinPage/JoinMember';
 import PostMain from '../pages/PostPage/PostMain/PostMain';
 import HomeBlank from '../pages/HomePage/HomePage';
+import { useRecoilValue } from 'recoil';
+import { authAtom } from '../atom/atoms';
 
 function Routing() {
+  const Login = useRecoilValue(authAtom);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartSplash />} />
-        <Route path="/homefeed" element={<HomeFeed />} />
-        <Route path="/home" element={<HomeBlank />} />
         <Route path="/loginmain" element={<LoginMain />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/joinmember" element={<JoinMember />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/chatroom" element={<ChatRoom />} />
-        <Route path="/myprofile" element={<MyProfilePage />} />
-        <Route path="/yourprofile" element={<YourProfilePage />} />
-        <Route path="/profileedit" element={<ProfileEditPage />} />
-        <Route path="/postmain" element={<PostMain />} />
-        <Route path="/postupload" element={<PostUploadPage />} />
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/search" element={<SearchPage />} />
+
+        {Login ? (
+          <Routes>
+            <Route path="/homefeed" element={<HomeFeed />} />
+            <Route path="/home" element={<HomeBlank />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/chatroom" element={<ChatRoom />} />
+            <Route path="/myprofile" element={<MyProfilePage />} />
+            <Route path="/yourprofile" element={<YourProfilePage />} />
+            <Route path="/profileedit" element={<ProfileEditPage />} />
+            <Route path="/postmain" element={<PostMain />} />
+            <Route path="/postupload" element={<PostUploadPage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/postmain" element={<PostMain />} />
+          </Routes>
+        ) : (
+          <Route path="/loginmain" element={<LoginPage />} />
+        )}
+
         <Route path="/404" element={<NotFound />} />
         <Route path="/test" element={<Test />} />
-        <Route path="/postmain" element={<PostMain />} />
       </Routes>
     </BrowserRouter>
   );
