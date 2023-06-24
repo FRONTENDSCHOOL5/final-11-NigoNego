68d1d8124840b73f0fc29dbf098fbb3480b8f577
leesua <thkWkddl100@gmail.com>
2023-06-24 16:46:09 +0900

    🐛 Fix :  yourprofile에서 yourPost 안뜨는 버그 해결

--- src/components/HomePost/YourHomePost.jsx
+++ src/components/HomePost/YourHomePost.jsx
@@ -3,54 +3,53 @@ import styled from 'styled-components';
 import msg from '../../assets/icons/message-icon.svg';
 import like from '../../assets/icons/like-icon.svg';
 import axios from 'axios';
+import UserSearch from '../common/User/UserSearch';
 
 export default function YourHomePost({ accountname }) {
   const [userData, setUserData] = useState(accountname);
-
+  const [userPost, setUserPost] = useState({});
   useEffect(() => {
-    if (userData) {
-      const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGFkMDkxYjJjYjIwNTY2MzM1ZjVmMCIsImV4cCI6MTY5MjAwMjk4NiwiaWF0IjoxNjg2ODE4OTg2fQ.IXRWQpeGB-5D3U3iN4FSKNf2F92wGVA_FLw4SpqLc20';
-      try {
-        axios({
-          method: 'GET',
-          url: `https://api.mandarin.weniv.co.kr/post/${userData}/userpost`,
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-type': 'application/json',
-          },
-        }).then(response => {
-          setUserData(response.data.post);
-          console.log(userData);
-        });
-      } catch (err) {
-        console.log('에러');
-      }
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGFkMDkxYjJjYjIwNTY2MzM1ZjVmMCIsImV4cCI6MTY5MjAwMjk4NiwiaWF0IjoxNjg2ODE4OTg2fQ.IXRWQpeGB-5D3U3iN4FSKNf2F92wGVA_FLw4SpqLc20';
+    try {
+      axios({
+        method: 'GET',
+        url: `https://api.mandarin.weniv.co.kr/post/${userData}/userpost`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      }).then(response => {
+        setUserPost(response.data.post);
+      });
+    } catch (err) {
+      console.log('에러');
     }
   }, [userData]);
 
   return (
     <>
-      {userData.map(data => {
-        return (
-          <>
-            <p>{data.content}</p>
-            {/* <p>{data.content}</p>
-            <HomePostImg src={data.image} />
-            <div>
-              <button type="button">
-                <img src={msg} alt="" />
-                <span>123</span>
-              </button>
-              <button type="button">
-                <img src={like} alt="" />
-                <span>123</span>
-              </button>
-            </div>
-            <time>{data.updatedAt}</time> */}
-          </>
-        );
-      })}
+      {userPost.length > 0 &&
+        userPost.map(data => {
+          return (
+            <>
+              {console.log(userPost)}
+              <UserSearch data={data} />
+              <p>{data.content}</p>
+              <HomePostImg src={data.image} />
+              <div>
+                <button type="button">
+                  <img src={msg} alt="" />
+                  <span>123</span>
+                </button>
+                <button type="button">
+                  <img src={like} alt="" />
+                  <span>123</span>
+                </button>
+              </div>
+            </>
+          );
+        })}
     </>
   );
 }
