1b368d8d5a420918eb73ed2601eddda1d53e7486
Oh5Yeonju <oh5yeonju@gmail.com>
2023-06-27 02:12:11 +0900

    ✨ Feat : #95 이미지정렬 네비바 기능 및 디자인

--- src/pages/ProfilePage/YourProfilePage/YourProfilePage.jsx
+++ src/pages/ProfilePage/YourProfilePage/YourProfilePage.jsx
@@ -1,27 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import YourProduct from '../../../components/Product/YourProduct';
 import YourHomePost from '../../../components/HomePost/YourHomePost';
 import YourProfileHeader from '../../../components/YourProfileHeader/YourProfileHeader';
-import { useLocation } from 'react-router-dom';
 import Navbar from '../../../components/common/Navbar/Navbar';
-import BodyGlobal from '../../../styles/BodyGlobal';
 import { HeaderBasicNav } from '../../../components/common/Header/Header';
-import { Testt } from './../../../api/getData/getData';
+import HomePostGrid from '../../../components/HomePost/HomePostGrid';
+
+import styled from 'styled-components';
+import BodyGlobal from '../../../styles/BodyGlobal';
+
+import { ReactComponent as BtnVertical } from '../../../assets/image/BtnVertical.svg';
+import { ReactComponent as BtnGrid } from '../../../assets/image/BtnGrid.svg';
 
 export default function YourProfilePage() {
   const location = useLocation();
   const accountname = location.state.accountname;
+
+  const [isClickedList, setIsClickedList] = useState(true);
+  const [isClickedGrid, setIsClickedGrid] = useState(false);
+
+  const handleClickList = e => {
+    e.preventDefault();
+    if (!isClickedList) {
+      setIsClickedList(true);
+      setIsClickedGrid(false);
+    }
+  };
+
+  const handleClickGrid = e => {
+    e.preventDefault();
+    if (!isClickedGrid) {
+      setIsClickedGrid(true);
+      setIsClickedList(false);
+    }
+  };
+
   return (
     <div>
       <HeaderBasicNav />
       <BodyGlobal>
         <YourProfileHeader accountname={accountname} />
-        {/* BodyGlobal 컴포넌트를 사용합니다. */}
         <YourProduct accountname={accountname} />
-        {console.log(accountname)}
-        <YourHomePost accountname={accountname} />
+
+        <HeaderDefaultStyle>
+          <button onClick={e => handleClickList(e)}>
+            <BtnGrid
+              width="26px"
+              height="26px"
+              fill={isClickedList ? '#767676' : '#dbdbdb'}
+              stroke={isClickedList ? '#767676' : '#dbdbdb'}
+            />
+          </button>
+          <button onClick={e => handleClickGrid(e)}>
+            <BtnVertical
+              width="26px"
+              height="26px"
+              fill={isClickedGrid ? '#767676' : '#dbdbdb'}
+              stroke={isClickedGrid ? '#767676' : '#dbdbdb'}
+            />
+          </button>
+        </HeaderDefaultStyle>
+
+        {isClickedList && <YourHomePost accountname={accountname} />}
+        {isClickedGrid && <HomePostGrid accountname={accountname} />}
       </BodyGlobal>
       <Navbar />
     </div>
   );
 }
+
+const HeaderDefaultStyle = styled.div`
+  height: 4.8rem;
+  border-top: 1px solid var(--basic-border-color);
+  border-bottom: 1px solid var(--basic-border-color);
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding: 0 10px;
+  background-color: #fff;
+  button {
+    border: 0;
+  }
+`;
