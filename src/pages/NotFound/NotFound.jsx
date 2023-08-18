9b0f36abdd282ecbb95e8df637be17b7e007cdd0
Oh5Yeonju <oh5yeonju@gmail.com>
2023-08-18 10:50:06 +0900

    ♻️ Refactor: #137 Layout 컴포넌트에 정렬 props 기능 추가

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import SVG from '../../assets/images/404 page.svg';
-import { Wrapper, Image, NotFoundText, NotFoundBtn } from './NotFoundStyle';
+import { Image, NotFoundText, NotFoundBtn } from './NotFoundStyle';
 import Layout from "../../styles/Layout";
 
 export default function NotFound() {
   return (
-    <Layout>
+    <Layout align="center">
       <Image src={SVG} alt="404" />
       <NotFoundText>페이지를 찾을 수 없음</NotFoundText>
       <NotFoundBtn>이전페이지</NotFoundBtn>
--- src/styles/Layout.jsx
+++ src/styles/Layout.jsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
-const Layout = styled.div`
+const Layout = styled.article`
   max-width: 390px;
   box-shadow: 4px 0 4px rgba(0, 0, 0, 0.1), -4px 0 4px rgba(0, 0, 0, 0.1); /* 양쪽에 흐림 효과를 줍니다 */
-  height: calc(var(--vh, 1vh) * 100);
+  height: 100vh;
   margin: 0 auto;
   box-sizing: border-box;
-  //padding: 48px 0 73px;
+  text-align: ${(props) => (props.align ? props.align : 'left')};
 `;
 
 export default Layout;
