58876e43d122c22190cd0fb35be6b55a14725243
snowkwon420 <snowkwon420@gmail.com>
2023-09-11 20:33:41 +0900

    ♻️ Refactor: #154 로그인 페이지 api 요청 커스텀훅으로 수정

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -11,6 +11,7 @@ import MainWrapperF from '../../styles/MainGlobal';
 import LoginApi from '../../api/getData/LoginApi';
 import { ButtonLong } from '../../components/common/button/Button';
 import Layout from '../../styles/Layout';
+import UseFetchToken from '../../Hooks/UseFetchToken';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -21,11 +22,12 @@ function LoginPage() {
   const [loginErrMessage, setLoginErrMessage] = useState('');
   const [isCorrect, setIsCorrect] = useState(null);
   const navigate = useNavigate();
-
   const [auth, setAuth] = useRecoilState(authAtom);
   const [accountname, setAccountname] = useRecoilState(accountNameAtom);
   const [following, setFollowing] = useRecoilState(followingAtom);
+  const { postLogin, getUserInfo } = UseFetchToken();
 
+  //이메일 검증 시작
   function emailCheck(event) {
     setEmail(event.target.value);
     const testEmail =
@@ -39,7 +41,9 @@ function LoginPage() {
       setIsEmailValid(false);
     }
   }
+  //이메일 검증 끝
 
+  //패스워드 validation 시작
   function passwordCheck(event) {
     setPassword(event.target.value);
     const testPassword = /^[A-Za-z0-9]{5,20}$/;
@@ -49,40 +53,46 @@ function LoginPage() {
       setIsPasswordValid(false);
     }
   }
+  //패스워드 validataion 끝
 
-  const url = 'https://api.mandarin.weniv.co.kr';
-
+  //여기서 then 과 await의 차이점이 뭔지 모르고 사용했음
+  //submit 버튼 시작//
   async function onhandlesubmit(event) {
     event.preventDefault();
-    LoginApi(
-      email,
-      password,
-      setAuth,
-      setAccountname,
-      FollowingData,
-      setIsCorrect,
-      setLoginErrMessage,
-    ).then(res => console.log(res));
+    await submitHandler();
+    followValid();
   }
+  //submit 버튼 끝//
 
-  async function FollowingData(token) {
-    try {
-      const res = await axios.get(`${url}/user/myinfo`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const { following } = res.data.user;
-      setFollowing(following);
-      if (Object.keys(following).length === 0) {
-        navigate('/home');
-      } else {
-        navigate('/homefeed');
-      }
-    } catch (error) {
-      console.error(error);
+  const newData = { user: { email: email, password: password } };
+
+  //postLogin 요청 시작//
+  const submitHandler = async () => {
+    console.log(newData);
+
+    await postLogin(newData).then(res => {
+      const { token, accountname } = res.data.user;
+      setAuth(token);
+      setAccountname(accountname);
+    });
+  };
+  //postLogin 요청 끝//
+
+  //getUserInfo API요청 시작//
+  //Follower 데이터가 없으면 home으로 있으면 homefeed로 가는 로직
+  const followValid = async () => {
+    console.log(auth);
+    const res = await getUserInfo();
+    const { following } = res.data.user;
+    setFollowing(following);
+
+    if (Object.keys(following).length === 0) {
+      navigate('/home');
+    } else {
+      navigate('/homefeed');
     }
-  }
+  };
+  //getUserInfo API요청 끝//
 
   return (
     <Layout>
