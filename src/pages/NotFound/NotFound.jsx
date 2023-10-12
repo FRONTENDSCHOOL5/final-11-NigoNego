fa935287fb0c6066d795c0b940a6964016798972
Oh5Yeonju <oh5yeonju@gmail.com>
2023-10-12 18:52:08 +0900

    fix: #183 404페이지 이전으로 버튼에 기능 추가

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import SVG from '../../assets/images/404 page.svg';
+import NotFoundImage from '../../assets/images/404 page.svg';
 import { Image, NotFoundText, NotFoundBtn } from './NotFoundStyle';
 import Layout from "../../styles/Layout";
+import {useNavigate} from "react-router-dom";
+import styled from "styled-components";
 
 export default function NotFound() {
-  return (
-    <Layout align="center">
-      <Image src={SVG} alt="404" />
-      <NotFoundText>페이지를 찾을 수 없음</NotFoundText>
-      <NotFoundBtn>이전페이지</NotFoundBtn>
-    </Layout>
-  );
+    const navigate = useNavigate();
+    const handleBack = () => {
+        navigate(-1);
+    }
+
+    return (
+        <Layout align="center">
+            <Image src={NotFoundImage} alt="404페이지 로고" />
+            <NotFoundText>페이지를 찾을 수 없음</NotFoundText>
+            <NotFoundBtn onClick={handleBack}>이전페이지</NotFoundBtn>
+        </Layout>
+    );
 }
